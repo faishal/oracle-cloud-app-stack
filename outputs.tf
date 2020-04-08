9ff31bd040094f171b0f15191d2649f27b38bd03
Faishal Saiyed <saiyedfaishal@gmail.com>
2020-04-08 12:48:36 -0400

    Added initial configuration

---
+++ outputs.tf
@@ -0,0 +1,3 @@
+output "show-ads" {
+  value = data.oci_identity_availability_domains.ads.availability_domains
+}
---
+++ variables.tf
@@ -0,0 +1,12 @@
+variable "tenancy_ocid" {
+}
+variable "user_ocid" {
+}
+variable "fingerprint" {
+}
+variable "private_key_path" {
+}
+variable "region" {
+}
+variable "private_key_password" {
+}
