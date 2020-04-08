111c86c34e4d4a8618f7a4e47f2918f1e89172b0
Faishal Saiyed <saiyedfaishal@gmail.com>
2020-04-08 13:18:02 -0400

    Added always_free instance code

--- variables.tf
+++ variables.tf
@@ -10,3 +10,6 @@ variable "region" {
 }
 variable "private_key_password" {
 }
+variable "ssh_public_key" {
+    default="ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAACAQC92NEAvX/XsRIk013CdTF1878m7zbKys131PBzadlP3Gd+68x3o/lc8yCdJ+c4yf/ydOb+fCCBFwZ71ncjBlx5TCjVNAzVq/LVHIkH9GSRzP7b+1T0m5V3KoVfBDdynrGlVFo3kqM95Cmzs2MPv5ABrPAD9yedDtyiGi5kBHXHxg/TI5g9nc7dZIU37nNQnUtxUTH1c2d+tOxiJihx3zTaVXTQM/iYXrr3+BIw3tJjX6ZRZWjzO9SF4IUOvoGHpnRH+4rd/dvZTH/fZy7bxdQJofvOtPcN4/XyGM2333HeuJ+p4ow6NSL+BLtY6taFS9J3qvInS5r8efmU63m4CkSaQqE53AeLdtygakVVADjAzvKEKlCVHxFHu+gS+fS+fR1IESItBxAxOpc3MRH+Bfrv1bF3AbuCrXzX+noDJpLzYEkGglF4zYZ+gM6pw5+2WepOpFnhKxG4wqrqh0HThG764TqIS6vhinNtCYKKY6bG5vzRb3NYak+J8/luG5h5UbGE0b305m0HHOgmxIzLDvZcb9/bFb57tNqGUjb3xnCeioPeMkEAfQH0DFmKQwSp81JJ4uZc3FtZsU1OpDNU9855+akFd9EaByRHcMPNJWPD8d0i3JkG3ItXQ3yMjtWfazEZzEqXk9b1k670God4c+oKEGrD7vs2w9kE9aLNCpO2RQ== me@faish.al"
+}
